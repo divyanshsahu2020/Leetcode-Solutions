d57c8d5225e36a01582d3903b8c12f27360d223b
Divyansh Sahu <80671629+divyanshsahu2020@users.noreply.github.com>
2023-09-08 15:37:18 +0530

    Time: 35 ms (90.92%), Space: 16.4 MB (5.86%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,21 +1,12 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        curr=[1]
-        ans=[]
-        ans.append(curr)
-        
-        sum=0
-        temp=[]
+        res=[[1]]
         for i in range(numRows-1):
-            summ_arr=[]
-            l=len(curr)
-            for j in range(l-1):
-                sum=curr[j]+curr[j+1]
-                summ_arr.append(sum)
-            curr=[curr[0]]+summ_arr+[curr[-1]]
-            ans.append(curr)
-        return(ans)
+            curr=res[-1]
+            temp=[curr[0]]
+            for j in range(0,len(curr)-1):
+                temp.append(curr[j]+curr[j+1])
+            temp.append(1)
+            res.append(temp)
+        return res
 
-
-
-        
